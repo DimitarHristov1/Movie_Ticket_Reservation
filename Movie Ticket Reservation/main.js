20336651c49a2e24a19141203412bbe53fce592b
DimitarHristov1 <138118685+DimitarHristov1@users.noreply.github.com>
2023-08-02 21:24:17 +0300

    Update main.js

--- Movie Ticket Reservation/main.js
+++ Movie Ticket Reservation/main.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 //Set minimum value date in datepicker as today
 var today = new Date().toISOString().split('T')[0];
 document.getElementById("date")[0].setAttribute('min', today);
